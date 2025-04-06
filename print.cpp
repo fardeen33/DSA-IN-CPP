56713c31d35e39052964f7ea2d31dfa3f6aa975c
fardeen33 <shahfardeen23@gmail.com>
2025-04-07 02:05:20 +0530

    first commit

---
+++ print.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    char b = 'a';
+    cout<<("Enter the ASCII code for letter 'a' ");
+    cin>>a;
+    cout<<(a == b);
+  
+  return 0;
+} 
---
+++ typecast.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int num = 100;
+  float b = 5.5;
+  float result = num + b;
+  cout<<"result is: "<<result;
+  return 0; // as per c++ standard a main fucntion must return something
+}
